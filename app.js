62d3522d3a639b5cc230f4b86851d80c03f9c0ff
roneelsi <roneel@sportzinteractive.net>
2019-12-17 01:35:43 +0530

    Captcha Changes

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 9000;
-const svgCaptcha = require("svg-captcha")
+const svgCaptcha = require("svg-captcha");
+const session = require('express-session')
+
+
+app.use(session({ secret: 'test', saveUninitialized: true, resave: true }));
 
 app.get('/getCaptcha', (req, res) => {
     const captcha = svgCaptcha.create();
-    res.json(captcha);
+    // req.session = {};    
+    // res.type('html')
+    // res.send(captcha.data)
+    // res.json(captcha.data);
+    req.session.captcha = captcha.text;
+    res.type('svg');
+    res.status(200).send(captcha.data);
 });
 
 app.get('/login', (req, res) => {
+    console.log(req.session)
     let status = 'Invalid Username or Password';
     if (req.query.username === 'admin' && req.query.password === 'admin') {
         status = 'Success';
